aaef19d31187518c062339258b052f4ea63ec2fd
Abhishek Shankar <abhisheksdevadiga271@gmail.com>
2024-03-12 10:21:05 +0530

    calculator app

---
+++ Projects/Calculator/script.js
@@ -0,0 +1,27 @@
+let input=document.getElementById('input');
+let button=document.querySelectorAll("button")
+
+
+button.forEach((Element)=>{
+    Element.addEventListener("click",function(e){
+        console.log(e.target.textContent)
+
+        if(e.target.textContent === "C"){
+            input.innerText=" ";
+        }
+        else if(e.target.textContent==="del"){
+            input.innerText=input.innerText.slice(0,-1);
+
+        }else if(e.target.textContent==="="){
+            input.innerText=eval(input.innerText)
+
+        }
+        
+        else{
+            input.innerText += e.target.textContent;
+        }
+        input.scrollLeft = input.scrollWidth;
+
+    })
+    
+})
