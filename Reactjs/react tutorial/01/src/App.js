ca9e9238c58de6f2bc36ea6f1e9460097dba2aa5
Abhishek Shankar <abhisheksdevadiga271@gmail.com>
2024-03-18 23:16:30 +0530

    React course

---
+++ Reactjs/react tutorial/01/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App(){
+    let a= 2;
+    return (
+        <div className='main'>
+            {a +2}
+            <h1>
+                Hello Im component
+            </h1>
+            <p>
+                hii
+            </p>
+        
+        </div>
+        
+    )
+}
+
+export default App;
